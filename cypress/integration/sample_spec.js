bcccc3f84a305de9a004317cb7f62d079dd76ccc
Nahiyan Kamal <noyanayihan@gmail.com>
2020-05-27 08:33:13 +0200

    auto generated message

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,10 +1,13 @@
 describe('Integration Tests', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080');
-  })
-
-  it('initial page should have no name shown', () => {
-    cy.get('.MuiCard-root').invoke('text').should('contain', '');
   });
 
+  it('initial page should have => 1.buttons, 2.Bug 3.Header Text', () => {
+    cy.contains('Left');
+    cy.contains('Right');
+    cy.contains('Go');
+
+    cy.contain('Move the bug to eat all food!');
+  });
 });

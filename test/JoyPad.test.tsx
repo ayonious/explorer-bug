58f18fa6fbe45523b0b95ef29d8dc5f26f83a4c0
Nahiyan Kamal <noyanayihan@gmail.com>
2020-07-13 21:55:10 +0200

    auto generated message

--- test/JoyPad.test.tsx
+++ test/JoyPad.test.tsx
@@ -1,5 +1,7 @@
-import * as React from 'react';
 import { render } from '@testing-library/react';
+import expect from 'expect';
+import React from 'react';
+
 import JoyPad from '../src/components/JoyPad';
 
 it('JoyPad renders', () => {
